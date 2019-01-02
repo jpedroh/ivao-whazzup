b89521ecb236f62bac5000cdf27928e2b76f4ba0
João Pedro Henrique <joao.pedro.hsd@gmail.com>
2019-01-02 15:48:59 -0200

    Corrected bug on clients build

--- src/utils/is-pilot.ts
+++ src/utils/is-pilot.ts
@@ -1,8 +1,7 @@
 /**
- * Check if the client is a pilot or not, by checking the field *OnGround* - pilot exclusive.
- * Checking field 3 doesn't always seems to work here.
+ * Check if the client is a pilot or not
  * @param clientData - Contains the client's raw data from the Whazzup file.
  */
 export function isPilot(clientData: string[]): boolean {
-    return clientData[46] === ''
+    return clientData[3] === 'PILOT'
 }
