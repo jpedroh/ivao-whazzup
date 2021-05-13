e570883ed3e52fbab2f7d9eb24d38bdec4679861
Joao Pedro <joao.pedro.hsd@gmail.com>
2021-05-13 10:49:29 -0300

    add git plugin

--- release.config.js
+++ release.config.js
@@ -3,6 +3,13 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
     '@semantic-release/github',
     '@semantic-release/npm',
   ],
